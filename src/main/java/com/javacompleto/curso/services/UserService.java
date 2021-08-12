67b6b89879a9085ae14dcb79e874ef2ca37ae51b
Willams Bernardo <willamsbernardo@hotmail.com>
2021-08-11 21:05:18 -0300

    Insercao de usuario com POST

--- src/main/java/com/javacompleto/curso/services/UserService.java
+++ src/main/java/com/javacompleto/curso/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javacompleto.curso.domain.User;
+import com.javacompleto.curso.dto.UserDTO;
 import com.javacompleto.curso.repository.UserRepository;
 import com.javacompleto.curso.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User inser(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
