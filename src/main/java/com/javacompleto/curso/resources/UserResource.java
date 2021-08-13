ef7e78836fed53880b8dd9b3646563624e1bd53b
Willams Bernardo <willamsbernardo@hotmail.com>
2021-08-13 20:28:30 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/javacompleto/curso/resources/UserResource.java
+++ src/main/java/com/javacompleto/curso/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,9 +43,8 @@ public class UserResource {
 	
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
-		
 		User obj = service.fromDTO(objDto);
-		obj = service.inser(obj);
+		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();				
 	}
@@ -53,6 +53,14 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.Delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();			
 	}
 	
 	
