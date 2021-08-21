5d476a2560d6c8237d63f2fdfe858294e177c29f
Willams Bernardo <willamsbernardo@hotmail.com>
2021-08-21 09:00:46 -0300

    Projecao com dados do autor com DTO

--- src/main/java/com/javacompleto/curso/config/Instantiation.java
+++ src/main/java/com/javacompleto/curso/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.javacompleto.curso.domain.Post;
 import com.javacompleto.curso.domain.User;
+import com.javacompleto.curso.dto.AuthorDTO;
 import com.javacompleto.curso.repository.PostRepository;
 import com.javacompleto.curso.repository.UserRepository;
 
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
